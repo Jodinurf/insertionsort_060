1bc33bbd72c8f03bdab08404245c2170465289bb
Jodinurf <jodinurfarkhani04@gmail.com>
2023-03-24 16:17:40 +0700

    membuat prosedur kendali program for dengan kondisi int i = 0; i<n; i++
    dan memunculkan tampilan "data ke-" dengan inputan (i+1)

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -21,3 +21,52 @@ void input() {
 	cout << "=====================" << endl;
 	cout << "Masukan Element Array" << endl;
 	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+}
+
+void insertionsort() {
+	int temp;
+	int j;
+
+	for (int i = 1; i < n; i++) {
+
+		temp = arr[i];
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
+
+		cout << "\nPass " << i << "; ";
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " ";
+		}
+	}
+}
+
+void display() {
+	cout << "\n===============================" << endl;
+	cout << "Element Array Yang Telah Tersusun" << endl;
+	cout << "=================================" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+}
+
+int main()
+{
+	input();
+	insertionsort();
+	display();
+}
